885fa488c719a1d6b84d4216ffd3cf7e41762b45
Victor Taing <153879417+vtaing10@users.noreply.github.com>
2025-03-21 11:01:26 -0400

    Create ContainDuplicate.py

---
+++ ContainDuplicate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        
+        
+        # dupSet = {}, if i use {}, i create an empty dictionary (not a set).  
+        dupSet = set() #this will allow me to .add() more items in it later (creates an empty set)
+        
+        for i in nums:
+            if i in dupSet:
+                return True
+            dupSet.add(i)
+        return False
+        
